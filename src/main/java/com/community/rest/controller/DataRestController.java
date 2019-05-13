81e8e276dc1273e8388e3923bb85d646cc3b91a6
doheeKang <doheee.kang@gmail.com>
2019-05-13 23:06:13 +0900

    Import naver map and denote specific place * change address to lat,lng * denote using marker

--- src/main/java/com/community/rest/controller/DataRestController.java
+++ src/main/java/com/community/rest/controller/DataRestController.java
@@ -1,16 +1,9 @@
 package com.community.rest.controller;
 
-import com.community.rest.domain.Merchant;
-import com.community.rest.domain.Trade;
-import com.community.rest.repository.MerchantRepository;
-import com.community.rest.repository.TradeRepositorty;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/map") //API URL 경로를 /map으로 정의
