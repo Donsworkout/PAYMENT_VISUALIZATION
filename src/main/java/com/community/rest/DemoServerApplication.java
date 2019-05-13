52e8ce339c874f538bad4cda4f3497b4193e15cc
dohee kang <doheekang@doheeui-MacBookPro.local>
2019-05-13 14:47:21 +0900

    initial commit * Setting for spring-boot-server

---
+++ src/main/java/com/community/rest/DemoServerApplication.java
@@ -0,0 +1,14 @@
+package com.community.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoServerApplication.class, args);
+
+    }
+}
