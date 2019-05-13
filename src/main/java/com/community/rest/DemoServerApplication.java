92502e80f4201d128ed4caac9d012fba6b28b658
doheeKang <doheee.kang@gmail.com>
2019-05-14 01:08:21 +0900

    Read all merchant address and denote at the map * Change address to LatLng * Donote using marker

--- src/main/java/com/community/rest/DemoServerApplication.java
+++ src/main/java/com/community/rest/DemoServerApplication.java
@@ -32,62 +32,62 @@ public class DemoServerApplication {
 
     }
 
-    @Bean
-    public CommandLineRunner runner(TradeRepositorty tradeRepositorty, MerchantRepository merchantRepository) throws Exception {
-        ReadOption excelReadOption = new ReadOption();
-        excelReadOption.setFilePath("/Users/doheekang/IdeaProjects/demo-server/src/main/resources/sample_data.xlsx");
-        excelReadOption.setOutputColumns("A", "B", "C", "D", "E", "F", "G", "H");
-        excelReadOption.setStartRow(2);
-
-
-        HashMap<Long, Merchant> merchantHashMap = new HashMap<>();
-
-
-        return (args) -> {
-            int sheetNum = 1;
-            List<Map<String, String>> excelContent;
-
-            while (true) {
-                excelContent = ExcelRead.read(excelReadOption, sheetNum);
-
-                if (excelContent == null)
-                    break;
-
-                if (sheetNum == 0) {
-                    for (Map<String, String> article : excelContent) {
-                        Trade trade = tradeRepositorty.save(Trade.builder()
-                                .id(Long.parseLong(article.get("A")))
-                                .tradeDate(article.get("B"))
-                                .tradeType(article.get("C"))
-                                .amount(article.get("D"))
-                                .fee(article.get("E"))
-                                .merchantId(merchantHashMap.get(Long.parseLong(article.get("F"))))
-                                .serviceType(article.get("G"))
-                                .tradeAccess(article.get("H"))
-                                .build());
-                    }
-
-                }
-                else if (sheetNum == 1) {
-                    for (Map<String, String> article : excelContent) {
-                        Merchant merchant = merchantRepository.save(Merchant.builder()
-                                .id(Long.parseLong(article.get("A")))
-                                .merchantName(article.get("B"))
-                                .regDate(article.get("C"))
-                                .serviceType(article.get("D"))
-                                .statusType(article.get("E"))
-                                .address(article.get("F"))
-                                .addressDetail(article.get("G"))
-                                .build());
-
-                        merchantHashMap.put(merchant.getId(), merchant);
-                    }
-                }
-
-                sheetNum--;
-            }
-        };
-    }
+//    @Bean
+//    public CommandLineRunner runner(TradeRepositorty tradeRepositorty, MerchantRepository merchantRepository) throws Exception {
+//        ReadOption excelReadOption = new ReadOption();
+//        excelReadOption.setFilePath("/Users/doheekang/IdeaProjects/demo-server/src/main/resources/sample_data.xlsx");
+//        excelReadOption.setOutputColumns("A", "B", "C", "D", "E", "F", "G", "H");
+//        excelReadOption.setStartRow(2);
+//
+//
+//        HashMap<Long, Merchant> merchantHashMap = new HashMap<>();
+//
+//
+//        return (args) -> {
+//            int sheetNum = 1;
+//            List<Map<String, String>> excelContent;
+//
+//            while (true) {
+//                excelContent = ExcelRead.read(excelReadOption, sheetNum);
+//
+//                if (excelContent == null)
+//                    break;
+//
+//                if (sheetNum == 0) {
+//                    for (Map<String, String> article : excelContent) {
+//                        Trade trade = tradeRepositorty.save(Trade.builder()
+//                                .id(Long.parseLong(article.get("A")))
+//                                .tradeDate(article.get("B"))
+//                                .tradeType(article.get("C"))
+//                                .amount(article.get("D"))
+//                                .fee(article.get("E"))
+//                                .merchantId(merchantHashMap.get(Long.parseLong(article.get("F"))))
+//                                .serviceType(article.get("G"))
+//                                .tradeAccess(article.get("H"))
+//                                .build());
+//                    }
+//
+//                }
+//                else if (sheetNum == 1) {
+//                    for (Map<String, String> article : excelContent) {
+//                        Merchant merchant = merchantRepository.save(Merchant.builder()
+//                                .id(Long.parseLong(article.get("A")))
+//                                .merchantName(article.get("B"))
+//                                .regDate(article.get("C"))
+//                                .serviceType(article.get("D"))
+//                                .statusType(article.get("E"))
+//                                .address(article.get("F"))
+//                                .addressDetail(article.get("G"))
+//                                .build());
+//
+//                        merchantHashMap.put(merchant.getId(), merchant);
+//                    }
+//                }
+//
+//                sheetNum--;
+//            }
+//        };
+//    }
 
 
     @Configuration
--- src/main/java/com/community/rest/controller/DataRestController.java
+++ src/main/java/com/community/rest/controller/DataRestController.java
@@ -1,5 +1,7 @@
 package com.community.rest.controller;
 
+import com.community.rest.repository.MerchantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/map") //API URL 경로를 /map으로 정의
 public class DataRestController {
 
+    @Autowired
+    MerchantRepository merchantRepository;
+
     @GetMapping({"","/"}) //매핑 경로
     public String board(Model model) {
-        model.addAttribute("data", "hello word");
+        model.addAttribute("merchantList", merchantRepository.findAll());
         return "/index";
     }
 }
