4851b12c2038c34625a0ead5c4fb36b101d3ae6a
doheeKang <doheee.kang@gmail.com>
2019-05-17 06:54:24 +0900

    dailyStatic 레파지토리, 서비스 오류 개선

--- src/main/java/com/community/rest/repository/DailyStaticRepository.java
+++ src/main/java/com/community/rest/repository/DailyStaticRepository.java
@@ -5,11 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface DailyRepository extends JpaRepository<DailyStatic, Long> {
-    List<DailyStatic> findByTradeDate(Date date);
-    Optional<DailyStatic> findByMerchantIdAndTradeDate(Long merchantId, Date tradeDate);
+public interface DailyStaticRepository extends JpaRepository<DailyStatic, Long> {
+    Optional<DailyStatic> findByMerchantIdAndTradeDate(Long id, Date tradeDate);
 }
