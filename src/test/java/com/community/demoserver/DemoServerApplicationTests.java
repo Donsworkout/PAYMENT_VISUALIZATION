3eb342763d6b00cae0d06e1dc818a997277c7ca5
donkim <dickings@likelion.org>
2019-05-26 19:58:33 +0900

    테스트 설정 변경

--- src/test/java/com/community/demoserver/DemoServerApplicationTests.java
+++ src/test/java/com/community/demoserver/DemoServerApplicationTests.java
@@ -2,11 +2,11 @@ package com.community.demoserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@ContextConfiguration
 public class DemoServerApplicationTests {
 
 	@Test
